b0fd3eaf3146caa11ee3ae1314b419a3d54aacf7
Ivan Ivanov <ivan.iliyanov95@gmail.com>
2023-05-28 15:41:34 +0300

    Create GuessTheNumber
    Created the GuessTheNumber class

---
+++ GuessTheNumber.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random randomNum = new Random();
+        int computerNumber = randomNum.nextInt(100);
+        while (true) {
+            System.out.print("Guess a number between 1 and 100: ");
+            String playerInput = scanner.nextLine();
+            int playerNumber;
+            boolean isValid = true;
+            for (int i = 0; i < playerInput.length(); i++) {
+                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                playerNumber = Integer.parseInt(playerInput);
+                if (playerNumber == computerNumber) {
+                    System.out.println("You guessed it!");
+                    break;
+                } else if (playerNumber > computerNumber) {
+                    System.out.println("Too High");
+                } else {
+                    System.out.println("Too Low");
+                }
+            } else {
+                System.out.println("Invalid Input.");
+            }
+        }
+    }
+}
